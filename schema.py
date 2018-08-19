837a514597b28300bbb5f52f53ffa1fc9816abf0
gleison <brittobaptista93@gmail.com>
2018-08-18 21:37:37 -0300

    initial

---
+++ schema.py
@@ -0,0 +1,100 @@
+from graphene import ObjectType, String, Boolean, ID, List, Field, Int, List, DateTime
+import arxiv
+import json
+import os
+from collections import namedtuple
+
+def _json_object_hook(d):
+    return namedtuple('X', d.keys())(*d.values())
+
+
+def json2obj(data):
+    return json.loads(data, object_hook=_json_object_hook)
+
+class SummaryDetail(ObjectType):
+    base = String()
+    language = String()
+    value = String()
+    type = String()
+
+class AuthorDetail(ObjectType):
+    name = String()
+
+class Link(ObjectType):
+    type = String()
+    rel = String()
+    href = String()
+
+class Tag(ObjectType):
+    label = String()
+    term = String()
+    scheme = String()
+
+class ArxivPrimaryCategory(ObjectType):
+    term = String()
+    scheme = String()
+
+class TitleDetail(ObjectType):
+    base = String()
+    language = String()
+    value = String()
+    type = String()
+
+class Entry(ObjectType):
+    summary_details = Field(SummaryDetail)
+    author = String()
+    id = ID()
+    author_detail = Field(AuthorDetail)
+    guidislink = Boolean()
+    published_parsed = List(Int)
+    pdf_url = String()
+    affiliation = String()
+    published = DateTime()
+    arxiv_comment = String()
+    links = Field(List(Link))
+    title = String()
+    journal_reference = String()
+    authors = List(String)
+    arxiv_url = String()
+    updated_parsed = List(Int)
+    doi = String()
+    tags = Field(List(Tag))
+    arxiv_primary_category = Field(ArxivPrimaryCategory)
+    title_detail = Field(TitleDetail)
+    updated = String()
+    summary = String()
+
+class Query(ObjectType):
+    entries = List(Entry, search_query=String(required=True), max_results=Int(required=True), start=Int(required=True),
+                sort_by=String(required=False), sort_order=String(required=False))
+    
+    def resolve_entries(self, info, search_query, max_results, start, sort_by, sort_order):
+        entries = arxiv.query(search_query=search_query,id_list=[],max_results=max_results, sort_by=sort_by, sort_order=sort_order)
+        return json2obj(json.dumps(entries))
+    
+    entry = List(Entry, id=ID(required=True))
+
+    def resolve_entry(self, info, id):
+        entry = arxiv.query(id_list=[id])
+        return json2obj(json.dumps(entry))    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
